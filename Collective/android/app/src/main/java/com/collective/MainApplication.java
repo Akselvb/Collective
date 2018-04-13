882dbda802b40aebaddf4ec7b0dab3d740e321a5
GeneralVincento <akselvb@gmail.com>
2018-04-13 14:02:22 +0200

    Installed some dependencies

--- Collective/android/app/src/main/java/com/collective/MainApplication.java
+++ Collective/android/app/src/main/java/com/collective/MainApplication.java
@@ -3,6 +3,8 @@ package com.collective;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.photoview.PhotoViewPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PhotoViewPackage(),
+            new VectorIconsPackage(),
             new SvgPackage(),
             new LinearGradientPackage()
       );
