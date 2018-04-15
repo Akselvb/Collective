3c74449ea84833ef19685ba21bf23b0264b583ed
GeneralVincento <akselvb@gmail.com>
2018-04-15 23:33:35 +0200

    Statusbar take account for iPhone X

--- Collective/src/components/common/StatusBarStyle.js
+++ Collective/src/components/common/StatusBarStyle.js
@@ -1,11 +1,5 @@
-import React, {
-  Component,
-} from 'react';
-import {
-  View,
-  StatusBar,
-  Platform,
-} from 'react-native';
+import React, { Component } from 'react';
+import { View, StatusBar, Platform, Dimensions } from 'react-native';
 
 const MyStatusBar = ({ backgroundColor, ...props }) => (
   <View style={[styles.statusBar, { backgroundColor }]}>
@@ -21,7 +15,20 @@ class StatusBarStyle extends Component {
   }
 }
 
-const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+let STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+
+//iPhone X brings trouble...
+const isIphoneX = () => {
+  const { height, width } = Dimensions.get('window');
+  return (
+    Platform.OS === 'ios' && (height === 812 || width === 812)
+  );
+};
+
+if (isIphoneX()) {
+  STATUSBAR_HEIGHT = 42;
+}
+
 const styles = {
   statusBar: {
     height: STATUSBAR_HEIGHT,
